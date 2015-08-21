9344f59a5817755f8ec3b9841295ea6bebd154db
esipeng <stiffme@gmail.com>
2015-08-21 16:58:01 +0800

    ClusterModule and test

--- src/main/scala/org/cluster/module/ClusterModule.scala
+++ src/main/scala/org/cluster/module/ClusterModule.scala
@@ -1,6 +1,6 @@
 package org.cluster.module
 
-import akka.actor.{ActorRef, Actor, FSM}
+import akka.actor.{ActorLogging, ActorRef, Actor, FSM}
 import scala.concurrent.duration._
 /**
  * Created by stiffme on 2015/8/20.
@@ -24,6 +24,10 @@ case object Terminated extends ClusterModuleState
 
 case object Faulty extends ClusterModuleState
 //data
+sealed trait ClusterModuleData
+case class CMDataSender(sender:ActorRef) extends ClusterModuleData
+case class CMDataHandover(sender:ActorRef,target:ActorRef) extends ClusterModuleData
+case object CMDataEmpty extends ClusterModuleData
 
 //case object CMEmpty extends ClusterModuleData
 //signal
@@ -37,51 +41,195 @@ case object SigCMTerminate
 case class SigCMTerminateResult(success:Boolean)
 private case object SigCMFaulty
 
-trait ClusterModule extends FSM[ClusterModuleState,ActorRef]{
+trait ClusterModule extends FSM[ClusterModuleState,ClusterModuleData] with ActorLogging{
   val moduleName:String
   def onInitialize():Boolean
   def onActivate():Boolean
-  def onHandover(target:Actor):Boolean
+  def onHandover(target:ActorRef):Boolean
   def onTerminate():Boolean
   def appReceive:PartialFunction[Any,Unit]
 
   //initial state: Uninitialized,CMEmpty
-  startWith(Uninitialized,_)
+  startWith(Uninitialized,CMDataEmpty)
 
   when(Uninitialized) {
-    case Event(SigCMInit,_) => {
+    case Event(SigCMInit,CMDataEmpty) => {
       val origin = sender()
-      goto(Initialized) using origin
+      goto(Initialized) using CMDataSender(origin)
     }
   }
 
   when(Initialized) {
-    case Event(SigCMInitResult(success)) => {
+    case Event(SigCMInitResult(success),_) => {
       if(success) stay()
       else goto(Faulty)
     }
     case Event(SigCMActivate,_) => {
       val origin = sender()
-      goto(Activated) using origin
+      goto(Activated) using CMDataSender(origin)
     }
   }
 
   when(Activated) {
-    case Event(SigCMActivateResult(success)) => {
+    case Event(SigCMActivateResult(success),_) => {
       if(success) stay()
       else goto(Faulty)
     }
-    case Event(SigCMHandover(target) ) => {
-      val oringin = sender()
-      goto(Handedover) using oringin
+    case Event(SigCMHandover(target),_ ) => {
+      val origin = sender()
+      goto(Handedover) using CMDataHandover(origin,target)
     }
-    case Event(SigCMTerminate ) => {
-      val oringin = sender()
-      goto(Terminated) using oringin
+    case Event(SigCMTerminate ,_) => {
+      val origin = sender()
+      goto(Terminated) using CMDataSender(origin)
+    }
+    case Event(e,_) =>  {
+      if(appReceive.isDefinedAt(e))
+        appReceive(e)
+      stay()
     }
   }
 
+  when(Handedover)  {
+    case Event(SigCMTerminate,_) => {
+      val origin = sender()
+      goto(Terminated) using CMDataSender(origin)
+    }
+    case Event(SigCMHandoverResult(success),_) => {
+      if (success) stay()
+      else goto(Faulty)
+    }
+    case Event(e,CMDataHandover(_,target)) => {
+      if(target != null)
+        target.forward(e)
 
+      stay()
+    }
+  }
 
+  when(Terminated)  {
+    case Event(SigCMTerminateResult(success), _) => {
+      if (success) stay()
+      else goto(Faulty)
+    }
+    case Event(e,s) =>  {
+      log.warning("Cluster module is already terminated, no further signal is allowed")
+      stay()
+    }
+  }
 
+  when(Faulty)  {
+    case Event(e,s) =>  {
+      log.warning("Cluster module is already faulty, no further signal is allowed")
+      stay()
+    }
+  }
+
+  //transitions definition
+  onTransition  {
+    case Uninitialized -> Initialized => {
+      nextStateData match {
+        case CMDataSender(sender) => {
+          try {
+            val success = onInitialize()
+            sender ! SigCMInitResult(success)
+            self ! SigCMInitResult(success)
+          } catch {
+            case e:Exception  => {
+              log.error("Exception initializing cluster module",e)
+              sender ! SigCMInitResult(false)
+              self ! SigCMInitResult(false)
+            }
+          }
+        }
+        case _ =>   log.error("Unexpected state data")
+      }
+    }
+
+    case Initialized -> Faulty => {}
+
+    case Initialized -> Activated => {
+      nextStateData match {
+        case CMDataSender(sender) => {
+          try {
+            val success = onActivate()
+            sender ! SigCMActivateResult(success)
+            self ! SigCMActivateResult(success)
+          } catch {
+            case e:Exception  => {
+              log.error("Exception Activating cluster module",e)
+              sender ! SigCMActivateResult(false)
+              self ! SigCMActivateResult(false)
+            }
+          }
+        }
+        case _ =>   log.error("Unexpected state data")
+      }
+    }
+
+    case Activated -> Faulty => {}
+
+    case Activated -> Handedover => {
+      nextStateData match {
+        case CMDataHandover(sender,target) => {
+          try {
+            val success = onHandover(target)
+            sender ! SigCMHandoverResult(success)
+            self ! SigCMHandoverResult(success)
+          } catch {
+            case e:Exception  => {
+              log.error("Exception Handing over cluster module",e)
+              sender ! SigCMHandoverResult(false)
+              self ! SigCMHandoverResult(false)
+            }
+          }
+        }
+        case _ =>   log.error("Unexpected state data")
+      }
+    }
+
+    case Activated -> Terminated => {
+      nextStateData match {
+        case CMDataSender(sender) => {
+          try {
+            val success = onTerminate()
+            sender ! SigCMTerminateResult(success)
+            self ! SigCMTerminateResult(success)
+          } catch {
+            case e:Exception  => {
+              log.error("Exception Terminating cluster module",e)
+              sender ! SigCMTerminateResult(false)
+              self ! SigCMTerminateResult(false)
+            }
+          }
+        }
+        case _ =>   log.error("Unexpected state data")
+      }
+    }
+
+    case Handedover -> Terminated => {
+      nextStateData match {
+        case CMDataSender(sender) => {
+          try {
+            val success = onTerminate()
+            sender ! SigCMTerminateResult(success)
+            self ! SigCMTerminateResult(success)
+          } catch {
+            case e:Exception  => {
+              log.error("Exception Terminating cluster module",e)
+              sender ! SigCMTerminateResult(false)
+              self ! SigCMTerminateResult(false)
+            }
+          }
+        }
+        case _ =>   log.error("Unexpected state data")
+      }
+    }
+
+    case _ -> Terminated => {
+      log.info("Cluster Module terminated")
+    }
+  }
+
+  initialize
 }
---
+++ src/test/scala/ClusterModuleTest.scala
@@ -0,0 +1,146 @@
+import akka.actor.{Actor, Props, ActorRef, ActorSystem}
+import akka.testkit.{EventFilter, ImplicitSender, TestKit}
+import com.typesafe.config.ConfigFactory
+import org.cluster.module._
+import org.scalatest.{BeforeAndAfterAll, Matchers, WordSpecLike}
+import scala.concurrent.duration._
+/**
+ * Created by esipeng on 8/21/2015.
+ */
+class ClusterModuleTest (_system:ActorSystem) extends TestKit(_system)
+  with ImplicitSender
+  with WordSpecLike
+  with Matchers
+  with BeforeAndAfterAll{
+
+  def this() = this(ActorSystem("ClusterModuleTest", ConfigFactory.parseString("""akka.loggers = ["akka.testkit.TestEventListener"] """)))
+
+  override def afterAll: Unit = {
+    TestKit.shutdownActorSystem(system)
+  }
+
+  class SuccessfulClusterModule extends ClusterModule {
+    override val moduleName: String = "SuccessfulClusterModule"
+
+    override def onInitialize(): Boolean = {log.info("onInitialize");true}
+
+    override def onActivate(): Boolean = {log.info("onActivate");true}
+
+    override def onHandover(target: ActorRef): Boolean = {log.info("onHandover");true}
+
+    override def appReceive: PartialFunction[Any, Unit] = {
+      case "echo" => sender ! "echoAck"
+    }
+
+    override def onTerminate(): Boolean = {log.info("onTerminate");true}
+  }
+
+  "Everything fine Cluster module" in {
+    val cm = system.actorOf(Props(new SuccessfulClusterModule))
+    EventFilter.info(message = "onInitialize",occurrences = 1) intercept  {
+      cm ! SigCMInit
+      expectMsg(SigCMInitResult(true))
+    }
+
+    EventFilter.info(message = "onActivate",occurrences = 1) intercept  {
+      cm ! SigCMActivate
+      expectMsg(SigCMActivateResult(true))
+    }
+
+    cm ! "echo"
+    expectMsg("echoAck")
+
+    EventFilter.info(message = "onHandover",occurrences = 1) intercept  {
+      cm ! SigCMHandover(target = null)
+      expectMsg(SigCMHandoverResult(true))
+    }
+
+    EventFilter.info(message = "onTerminate",occurrences = 1) intercept  {
+      cm ! SigCMTerminate
+      expectMsg(SigCMTerminateResult(true))
+    }
+  }
+
+  "No app message handled when Cluster module is not in activated state"  {
+    val cm = system.actorOf(Props(new SuccessfulClusterModule))
+    EventFilter.warning("unhandled event echo in state Uninitialized") intercept {
+      cm ! "echo"
+      expectNoMsg(500 milliseconds)
+    }
+
+
+    EventFilter.info(message = "onInitialize",occurrences = 1) intercept  {
+      cm ! SigCMInit
+      expectMsg(SigCMInitResult(true))
+    }
+
+    EventFilter.warning("unhandled event echo in state Initialized") intercept {
+      cm ! "echo"
+      expectNoMsg(500 milliseconds)
+    }
+
+    EventFilter.info(message = "onActivate",occurrences = 1) intercept  {
+      cm ! SigCMActivate
+      expectMsg(SigCMActivateResult(true))
+    }
+
+    cm ! "echo"
+    expectMsg("echoAck")
+
+    EventFilter.info(message = "onHandover",occurrences = 1) intercept  {
+      cm ! SigCMHandover(target = null)
+      expectMsg(SigCMHandoverResult(true))
+    }
+
+    cm ! "echo"
+    expectNoMsg(500 milliseconds)
+
+    EventFilter.info(message = "onTerminate",occurrences = 1) intercept  {
+      cm ! SigCMTerminate
+      expectMsg(SigCMTerminateResult(true))
+    }
+
+    EventFilter.warning("Cluster module is already terminated, no further signal is allowed") intercept {
+      cm ! "echo"
+      expectNoMsg(500 milliseconds)
+    }
+    0
+  }
+
+
+  "App messages are forwarded to target after handover " in {
+    class ActorTarget extends Actor {
+      def receive = {
+        case "echo" => sender ! "echoAck2"
+      }
+    }
+
+    val cm = system.actorOf(Props(new SuccessfulClusterModule))
+    val target = system.actorOf(Props(new ActorTarget))
+    EventFilter.info(message = "onInitialize",occurrences = 1) intercept  {
+      cm ! SigCMInit
+      expectMsg(SigCMInitResult(true))
+    }
+
+    EventFilter.info(message = "onActivate",occurrences = 1) intercept  {
+      cm ! SigCMActivate
+      expectMsg(SigCMActivateResult(true))
+    }
+
+    cm ! "echo"
+    expectMsg("echoAck")
+
+    EventFilter.info(message = "onHandover",occurrences = 1) intercept  {
+      cm ! SigCMHandover(target = target)
+      expectMsg(SigCMHandoverResult(true))
+    }
+
+    cm ! "echo"
+    expectMsg("echoAck2")
+
+    EventFilter.info(message = "onTerminate",occurrences = 1) intercept  {
+      cm ! SigCMTerminate
+      expectMsg(SigCMTerminateResult(true))
+    }
+  }
+}
